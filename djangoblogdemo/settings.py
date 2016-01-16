478262186ffa696b379717ab0f313604fda5c60e
ScarecrowFu <fualan@foxmail.com>
2016-01-16 20:59:58 +0800

    master

--- djangoblogdemo/settings.py
+++ djangoblogdemo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pl2%fj+s7sa^6(qu21uog)xn%1mulu5g1+jipvvlqpn%=gq2#j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
